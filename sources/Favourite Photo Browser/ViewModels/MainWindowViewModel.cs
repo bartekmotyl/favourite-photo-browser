52bc9db84f13a6a90afccb6ffbe4775a4134bcb3
bartekmotyl <29923773+bartekmotyl@users.noreply.github.com>
2022-09-06 21:46:23 +0200

    mvvm part2

--- sources/Favourite Photo Browser/ViewModels/MainWindowViewModel.cs
+++ sources/Favourite Photo Browser/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,10 @@
 ﻿using Avalonia.Collections;
+using Avalonia.Media.Imaging;
+using Avalonia.Threading;
+using ReactiveUI;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,8 +13,98 @@ namespace Favourite_Photo_Browser.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
+        private readonly DBConnector dbConnector;
+        private ThumnailsLoadingJob? thumbnailsLoadingJob = null;
+
         private readonly AvaloniaList<FolderItemViewModel> folderItems = new();
+        private string currentFolderPath = "(not slected)";
+        private FolderItemViewModel? currentFolderItem = null;
 
         public AvaloniaList<FolderItemViewModel> FolderItems => folderItems;
+        public string CurrentFolderPath { get => currentFolderPath; set => this.RaiseAndSetIfChanged(ref currentFolderPath, value); }
+        public FolderItemViewModel? CurrentFolderItem { get => currentFolderItem; set => this.RaiseAndSetIfChanged(ref currentFolderItem, value); }
+
+
+        public MainWindowViewModel()
+        {
+            this.dbConnector = new DBConnector(@"photos.db");
+        }
+
+        public void ToggleFavourite()
+        {
+            if (currentFolderItem == null)
+                return;
+
+            Task.Run(async () =>
+            {
+                var updated = await dbConnector.ToggleFavourite(currentFolderItem!.ImageId!.Value);
+                currentFolderItem.Favourite = updated;
+            });
+        }
+
+
+        public async Task LoadFilesInFolder(string folderPath)
+        {
+
+            var directory = new DirectoryInfo(folderPath);
+            var files = directory.GetFiles().OrderBy(f => f.CreationTimeUtc).ToList();
+
+
+
+            var allFolderItems = files.Select(fileInfo => new FolderItemViewModel(fileInfo.FullName, fileInfo.Name)).ToList();
+
+
+            await Dispatcher.UIThread.InvokeAsync(() =>
+            {
+                FolderItems.Clear();
+                FolderItems.AddRange(allFolderItems);
+            });
+            this.thumbnailsLoadingJob?.CancellationTokenSource.Cancel();
+
+
+
+            var fileNamesToProcess = allFolderItems.Where(fi => !fi.Ignored).Select(fi => fi.FileName).ToArray();
+            this.thumbnailsLoadingJob = new ThumnailsLoadingJob(folderPath, fileNamesToProcess);
+
+            var readThumnailsTask = dbConnector.ReadThumbnails(thumbnailsLoadingJob);
+
+            var processLoadedImagesAsync = () =>
+            {
+                while (!thumbnailsLoadingJob.ProcessingProgress.IsEmpty)
+                {
+                    if (thumbnailsLoadingJob.ProcessingProgress.TryDequeue(out var thumbnailLoadingStatus))
+                    {
+
+                        var thumbnail = folderItems!.FirstOrDefault(t => t.Title == thumbnailLoadingStatus.FileName);
+                        if (thumbnail != null)
+                        {
+                            var data = thumbnailLoadingStatus.LoadedThumbnail!.Data;
+                            var bitmap = new Bitmap(new MemoryStream(data));
+                            thumbnail.ThumbnailImage = bitmap;
+                            thumbnail.IsLoaded = true;
+                            thumbnail.ImageId = thumbnailLoadingStatus.LoadedThumbnail!.ImageId;
+                            thumbnail.Favourite = thumbnailLoadingStatus.LoadedThumbnail!.Favourite;
+                        }
+
+                    };
+                }
+            };
+
+
+            while (!readThumnailsTask.IsCompleted)
+            {
+                if (thumbnailsLoadingJob.CancellationTokenSource.IsCancellationRequested)
+                    break;
+
+                if (!thumbnailsLoadingJob.ProcessingProgress.IsEmpty)
+                    processLoadedImagesAsync();
+                else
+                    await Task.Delay(100);
+            }
+
+            if (!thumbnailsLoadingJob.ProcessingProgress.IsEmpty)
+                processLoadedImagesAsync();
+        }
+
     }
 }
--- sources/Favourite Photo Browser/Views/MainWindow.axaml.cs
+++ sources/Favourite Photo Browser/Views/MainWindow.axaml.cs
@@ -23,25 +23,21 @@ namespace Favourite_Photo_Browser
 
     public partial class MainWindow : Window
     {
-        private string? currentFolder = null;
-        private readonly DBConnector dbConnector;
-        private ThumnailsLoadingJob? thumbnailsLoadingJob = null;
-        private FolderItemViewModel? currentFolderItem = null;
 
-        private readonly Subject<string> currentFolderPath = new();
+
+
+
+
         private MainWindowViewModel? ViewModel => (MainWindowViewModel?)DataContext;
 
         public MainWindow()
         {
             InitializeComponent();
-            this.dbConnector = new DBConnector(@"photos.db");
-            
+  
             this.KeyDown += MainWindow_KeyDown;
             
             zoomBorderImage.KeyDown += ZoomBorderImage_KeyDown;
             WindowState = WindowState.Maximized;
-            textCurrentFolderPath.Bind(TextBlock.TextProperty, currentFolderPath);
-            currentFolderPath.OnNext("(not selected)");
         }
 
         private void MainWindow_KeyDown(object? sender, KeyEventArgs e)
@@ -61,7 +57,7 @@ namespace Favourite_Photo_Browser
                     zoomBorderImage.ResetMatrix();
                     break;
                 case Key.F:
-                    ToggleFavourite();
+                    ViewModel?.ToggleFavourite();
                     break;
             }
         }
@@ -93,8 +89,8 @@ namespace Favourite_Photo_Browser
             if (thumbnailData.Ignored)
                 return;
 
-            if (currentFolderItem != null)
-                currentFolderItem.IsActive = false;
+            if (ViewModel!.CurrentFolderItem != null)
+                ViewModel!.CurrentFolderItem.IsActive = false;
             
             var index = ViewModel!.FolderItems.IndexOf(thumbnailData);
             SetCurrentImage(index);
@@ -107,17 +103,10 @@ namespace Favourite_Photo_Browser
         }
         private void FavouriteToggle_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
         {
-            ToggleFavourite();
+            ViewModel?.ToggleFavourite();
         }
 
-        private void ToggleFavourite()
-        {
-            Task.Run(async () =>
-            {
-                var updated = await dbConnector.ToggleFavourite(currentFolderItem!.ImageId!.Value);
-                currentFolderItem.Favourite = updated;
-            });
-        }
+
         private async void OpenFolder()
         {
             var folders = await StorageProvider.OpenFolderPickerAsync(new FolderPickerOpenOptions()
@@ -134,91 +123,22 @@ namespace Favourite_Photo_Browser
             if (!folder.TryGetUri(out folderUri))
                 return;
 
-            this.currentFolder = folderUri.AbsolutePath;
+            ViewModel!.CurrentFolderPath = folderUri.AbsolutePath;
 
             thumbnailsScrollViewer.Offset = Avalonia.Vector.Zero;
+            var viewModel = ViewModel!;
 
             await Task.Run(async () =>
             {
-                await LoadFilesInFolder();
+                await viewModel.LoadFilesInFolder(folderUri.AbsolutePath);
             });
         }
 
-        private async Task LoadFilesInFolder()
-        {
-            
-            var directory = new DirectoryInfo(this.currentFolder!);
-            var files = directory.GetFiles().OrderBy(f => f.CreationTimeUtc).ToList();
-            
-
-            
-            var allFolderItems = files.Select(fileInfo => new FolderItemViewModel(fileInfo.FullName, fileInfo.Name)).ToList();
-
-
-            await Dispatcher.UIThread.InvokeAsync(() =>
-            {
-                ViewModel!.FolderItems.Clear();
-                ViewModel!.FolderItems.AddRange(allFolderItems);
-                currentFolderPath.OnNext(this.currentFolder ?? "");
-                //this.textCurrentFolder.Text = this.currentFolder;
-            });
-            this.thumbnailsLoadingJob?.CancellationTokenSource.Cancel();
-
-
-
-            var fileNamesToProcess = allFolderItems.Where(fi => !fi.Ignored).Select(fi => fi.FileName).ToArray();
-            this.thumbnailsLoadingJob = new ThumnailsLoadingJob(this.currentFolder!, fileNamesToProcess);
-
-            AvaloniaList<FolderItemViewModel>? folderItems = null;
-
-            await Dispatcher.UIThread.InvokeAsync(() =>
-            {
-                folderItems = ViewModel!.FolderItems;
-            });
-
-
-            var readThumnailsTask = dbConnector.ReadThumbnails(thumbnailsLoadingJob);
-
-            var processLoadedImagesAsync = () =>
-            {
-                while (!thumbnailsLoadingJob.ProcessingProgress.IsEmpty)
-                {
-                    if (thumbnailsLoadingJob.ProcessingProgress.TryDequeue(out var thumbnailLoadingStatus))
-                    {
-
-                        var thumbnail = folderItems!.FirstOrDefault(t => t.Title == thumbnailLoadingStatus.FileName);
-                        if (thumbnail != null)
-                        {
-                            var data = thumbnailLoadingStatus.LoadedThumbnail!.Data;
-                            var bitmap = new Bitmap(new MemoryStream(data));
-                            thumbnail.ThumbnailImage = bitmap;
-                            thumbnail.IsLoaded = true;
-                            thumbnail.ImageId = thumbnailLoadingStatus.LoadedThumbnail!.ImageId;
-                            thumbnail.Favourite = thumbnailLoadingStatus.LoadedThumbnail!.Favourite;
-                        }
-
-                    };
-                }
-            };
-
-
-            while (!readThumnailsTask.IsCompleted)
-            {
-                if (thumbnailsLoadingJob.CancellationTokenSource.IsCancellationRequested)
-                    break;
-
-                if (!thumbnailsLoadingJob.ProcessingProgress.IsEmpty)
-                    processLoadedImagesAsync();
-                else
-                    await Task.Delay(100);
-            }
-
-            if (!thumbnailsLoadingJob.ProcessingProgress.IsEmpty)
-                processLoadedImagesAsync();
-        }
 
         private void NavigateToImage(int offset)
         {
+            var currentFolderItem = ViewModel!.CurrentFolderItem;
+
             if (currentFolderItem == null)
                 return;
             var index = ViewModel!.FolderItems.IndexOf(currentFolderItem);
@@ -238,37 +158,41 @@ namespace Favourite_Photo_Browser
             }
             SetCurrentImage(newIndex);
         }
-        private async Task EnsureItemVisibleInScrollViewer(int index)
+        private void EnsureItemVisibleInScrollViewer(int index)
         {
-            await Dispatcher.UIThread.InvokeAsync(() =>
+            // TODO: fix the code below - it doesn't seem to work with Avalonia 11
+            /*
+            var control = thumbnailsItemRepeater.TryGetElement(0);
+            if (control != null)
             {
-                var control = thumbnailsItemRepeater.TryGetElement(index);
-                if (control != null)
+                var scrollWindowWidth = thumbnailsScrollViewer.Bounds.Width;
+                if (control.Bounds.Left > scrollWindowWidth + thumbnailsScrollViewer.Offset.X)
                 {
-                    var scrollWindowWidth = thumbnailsScrollViewer.Bounds.Width;
-                    if (control.Bounds.Left > scrollWindowWidth + thumbnailsScrollViewer.Offset.X)
-                    {
-                        thumbnailsScrollViewer.Offset = new Vector(control.Bounds.Left - scrollWindowWidth / 2, 0);
-                    }
-                    if (control.Bounds.Right < thumbnailsScrollViewer.Offset.X)
-                    {
-                        thumbnailsScrollViewer.Offset = new Vector(control.Bounds.Left - scrollWindowWidth / 2, 0);
-                    }
+                    thumbnailsScrollViewer.Offset = new Vector(control.Bounds.Left - scrollWindowWidth / 2, 0);
                 }
-            });
+                if (control.Bounds.Right < thumbnailsScrollViewer.Offset.X)
+                {
+                    thumbnailsScrollViewer.Offset = new Vector(control.Bounds.Left - scrollWindowWidth / 2, 0);
+                }
+            }
+            */
         }
 
         private void SetCurrentImage(int index)
         {
-            if (currentFolderItem != null)
-                currentFolderItem.IsActive = false;
 
-            currentFolderItem = ViewModel!.FolderItems[index];
+            if (ViewModel!.CurrentFolderItem != null)
+                ViewModel!.CurrentFolderItem.IsActive = false;
+
+            ViewModel!.CurrentFolderItem = ViewModel!.FolderItems[index];
+
+            var currentFolderItem = ViewModel!.CurrentFolderItem;
             currentFolderItem.IsActive = true;
 
+            EnsureItemVisibleInScrollViewer(index);
+            
             Task.Run(async () =>
             {
-                await EnsureItemVisibleInScrollViewer(index);
 
                 var pathToLoad = currentFolderItem.Path;
                 var bitmap = new Bitmap(pathToLoad); // this takes time, especially on network drive 
@@ -279,7 +203,6 @@ namespace Favourite_Photo_Browser
                     {
                         zoomBorderImage.ResetMatrix();
                         targetImage.Source = bitmap;
-                        iconCurrentImageFavourite.DataContext = currentFolderItem;
                     });
                 }
             });
